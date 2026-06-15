2e4e046c1c3a4806ca64686d2d95f7f16c3ee244
Luiz Felipe Arcanjo Rangel <luiz.rangel@alunos.unis.edu.br>
2026-06-15 16:55:54 -0300

    chore: move ping route to api

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TelegramWebhookController;
 
 Route::post('/webhook/telegram', [TelegramWebhookController::class, 'handle']);
+
+Route::get('/ping', function () {
+    return response('pong', 200);
+});
